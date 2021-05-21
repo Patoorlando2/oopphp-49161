10bf38a0e61f84f5b76c692749197573572982e0
Marcos <exegeses@gmail.com>
2021-05-21 20:33:13 -0300

    repaso mysqli

---
+++ clase2/funciones.php
@@ -0,0 +1,21 @@
+<?php
+
+    function conectar()
+    {
+        $link = mysqli_connect(
+            'localhost',
+            'root',
+            '',
+            'agencia'
+        );
+        return $link;
+    }
+
+    function listarRegiones()
+    {
+        $link = conectar();
+        $sql = "SELECT regID, regNombre FROM regiones";
+        $resultado = mysqli_query($link, $sql);
+        $regiones = mysqli_fetch_assoc($resultado);
+        return $regiones;
+    }
