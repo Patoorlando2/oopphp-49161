e9a4587a5720c6b3429649ad1eaed4c9edef9598
Marcos <exegeses@gmail.com>
2021-06-04 20:28:02 -0300

    Aplicación de modificación de un destino

--- agencia/formModificarDestino.php
+++ agencia/formModificarDestino.php
@@ -74,7 +74,8 @@
                         </div>
                     </div>
 
-
+                    <input type="hidden" name="destID"
+                           value="<?= $Destino->getDestID() ?>">
                     <button class="btn btn-dark mr-3">Modificar destino</button>
                     <a href="adminDestinos.php" class="btn btn-outline-secondary">
                         Volver a panel de destinos
